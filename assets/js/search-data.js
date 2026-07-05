859ca0c6a39b003d1f1da0036c49aa1a8cda3e7e
utkarsh-2000 <63909991+utkarsh-2000@users.noreply.github.com>
2026-07-05 11:54:10 +0000

    Deploying to gh-pages from @ utkarsh-2000/utkarsh-2000.github.io@fe7a6fcbf287289af1ca0141eeeb4a3fbeecf547 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -399,6 +399,17 @@ ninja.data = [{
             window.location.href = "/blog/2015/code/";
           
         },
+      },{id: "post-my-first-blog-post",
+        
+          title: "My first blog post",
+        
+        description: "this is what included images could look like",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2015/first_blog/";
+          
+        },
       },{id: "post-a-post-with-images",
         
           title: "a post with images",
